cdf90c8e3c14f28656941ddc85b1cd955c023d71
BrazhnikDA <M1ND228@yandex.ru>
2021-12-13 15:50:42 +0300

    Authorization

---
+++ app/src/main/java/com/harman/imageprocessing/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.harman.imageprocessing.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
