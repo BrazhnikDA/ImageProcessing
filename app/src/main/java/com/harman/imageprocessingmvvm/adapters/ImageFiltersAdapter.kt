6be2edbec0a217b24aca4d602e6ab2743563e0c8
BrazhnikDA <M1ND228@yandex.ru>
2022-01-21 22:03:07 +0300

    Add border on selected filter

--- app/src/main/java/com/harman/imageprocessingmvvm/adapters/ImageFiltersAdapter.kt
+++ app/src/main/java/com/harman/imageprocessingmvvm/adapters/ImageFiltersAdapter.kt
@@ -1,8 +1,11 @@
 package com.harman.imageprocessingmvvm.adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.harman.imageprocessingmvvm.R
 import com.harman.imageprocessingmvvm.data.ImageFilter
 import com.harman.imageprocessingmvvm.databinding.ItemContainerFilterBinding
 import com.harman.imageprocessingmvvm.listeners.ImageFilterListener
@@ -10,7 +13,10 @@ import com.harman.imageprocessingmvvm.listeners.ImageFilterListener
 class ImageFiltersAdapter(
     private val imageFilters: List<ImageFilter>,
     private val imageFilterListener: ImageFilterListener
-    ) : RecyclerView.Adapter<ImageFiltersAdapter.ImageFilterViewHolder>() {
+) : RecyclerView.Adapter<ImageFiltersAdapter.ImageFilterViewHolder>() {
+
+    private var selectedFilterPosition = 0
+    private var previouslySelectedPosition = 0
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageFilterViewHolder {
         val binding = ItemContainerFilterBinding.inflate(
@@ -19,15 +25,44 @@ class ImageFiltersAdapter(
         return ImageFilterViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ImageFilterViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ImageFilterViewHolder, @SuppressLint("RecyclerView") position: Int) {
         with(holder) {
             with(imageFilters[position]) {
                 binding.imageFilterPreview.setImageBitmap(filterPreview)
                 binding.textFilterName.text = name
                 binding.root.setOnClickListener {
-                    imageFilterListener.onFilterSelected(this)
+                    if (position != selectedFilterPosition) {
+                        imageFilterListener.onFilterSelected(this)
+                        previouslySelectedPosition = selectedFilterPosition
+                        selectedFilterPosition = position
+                        with(this@ImageFiltersAdapter) {
+                            notifyItemChanged(previouslySelectedPosition, Unit)
+                            notifyItemChanged(selectedFilterPosition, Unit)
+                        }
+                    }
+
                 }
             }
+            binding.textFilterName.setTextColor(
+                ContextCompat.getColor(
+                    binding.textFilterName.context,
+                    if (selectedFilterPosition == position) {
+                        R.color.selectedFilter
+                    } else {
+                        R.color.primaryText
+                    }
+                )
+            )
+            binding.imageFilterPreview.setBackgroundColor(
+                ContextCompat.getColor(
+                    binding.textFilterName.context,
+                    if (selectedFilterPosition == position) {
+                        R.color.black
+                    } else {
+                        R.color.white
+                    }
+                )
+            )
         }
     }
 
