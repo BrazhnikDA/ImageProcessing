2049cd4ebeab0a671d69656c3a917d6ff42e2012
BrazhnikDA <M1ND228@yandex.ru>
2022-01-27 13:12:04 +0300

    Added simple documentation

--- app/src/main/java/com/harman/imageprocessingmvvm/dependencyinjection/RepositoryModule.kt
+++ app/src/main/java/com/harman/imageprocessingmvvm/dependencyinjection/RepositoryModule.kt
@@ -7,6 +7,9 @@ import com.harman.imageprocessingmvvm.repositories.SavedImagesRepositoryImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
+/**
+ * Object Factory
+ */
 val repositoryModule = module {
     factory<EditImageRepository> { EditImageRepositoryImpl(androidContext()) }
     factory<SavedImagesRepository> { SavedImagesRepositoryImpl(androidContext()) }
--- app/src/main/java/com/harman/imageprocessingmvvm/utilities/Extensions.kt
+++ app/src/main/java/com/harman/imageprocessingmvvm/utilities/Extensions.kt
@@ -4,10 +4,16 @@ import android.content.Context
 import android.view.View
 import android.widget.Toast
 
+/**
+ * Display message
+ */
 fun Context.displayToast(message: String?) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
+/**
+ * Changing visibility
+ */
 fun View.show() {
     this.visibility = View.VISIBLE
 }
