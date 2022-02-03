145cdcffcce40f5c3c886c948de216d01fc2e1da
BrazhnikDA <M1ND228@yandex.ru>
2022-02-03 13:28:25 +0300

    fun save image gallery

--- app/src/main/java/com/harman/imageprocessingmvvm/activities/filteredimage/FilteredImageActivity.kt
+++ app/src/main/java/com/harman/imageprocessingmvvm/activities/filteredimage/FilteredImageActivity.kt
@@ -1,11 +1,23 @@
 package com.harman.imageprocessingmvvm.activities.filteredimage
 
+import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.drawToBitmap
+import com.davemorrissey.labs.subscaleview.ImageSource
 import com.harman.imageprocessingmvvm.activities.editimage.EditImageActivity
 import com.harman.imageprocessingmvvm.databinding.ActivityFilteredImageBinding
+import com.harman.imageprocessingmvvm.utilities.displayToast
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+
 
 class FilteredImageActivity : AppCompatActivity() {
 
@@ -18,12 +30,16 @@ class FilteredImageActivity : AppCompatActivity() {
         setContentView(binding.root)
         displayFilteredImage()
         setListeners()
+
+        intent.getStringExtra("draft")?.let {
+            displayToast(it)
+        }
     }
 
     private fun displayFilteredImage() {
         intent.getParcelableExtra<Uri>(EditImageActivity.KEY_FILTERED_IMAGE_URI)?.let { imageUri ->
             fileUri = imageUri
-            binding.imageFilteredImage.setImageURI(imageUri)
+            binding.imageFilteredImage.setImage(ImageSource.uri(imageUri))
         }
     }
 
@@ -36,8 +52,60 @@ class FilteredImageActivity : AppCompatActivity() {
                 startActivity(this)
             }
         }
-        binding.imageBack.setOnClickListener{
+
+        binding.buttonSaveImage.setOnClickListener {
+            MediaStore.Images.Media.getBitmap(this.contentResolver, fileUri).apply {
+                saveMediaToStorage(this)
+            }
+            displayToast("Image Saved!")
+        }
+
+        binding.imageBack.setOnClickListener {
             onBackPressed()
+        }
+    }
+
+    // Method to save an image to gallery and return uri
+    private fun saveMediaToStorage(bitmap: Bitmap) {
+        //Generating a file name
+        val filename = "${System.currentTimeMillis()}.jpg"
+
+        //Output stream
+        var fos: OutputStream? = null
+
+        //For devices running android >= Q
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //getting the contentResolver
+            this.contentResolver?.also { resolver ->
+
+                //Content resolver will process the content values
+                val contentValues = ContentValues().apply {
+
+                    //putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                //Inserting the contentValues to contentResolver and getting the Uri
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                //Opening an outputstream with the Uri that we got
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            //These for devices running on android < Q
+            //So I don't think an explanation is needed here
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            //Finally writing the bitmap to the output stream that we opened
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
         }
     }
 }
